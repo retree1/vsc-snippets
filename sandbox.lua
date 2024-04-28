7c31b177c8feebab2b4bc6c0c491e18b72f17dac
retree1 <70078437+retree1@users.noreply.github.com>
2024-04-28 11:03:16 -0400

    secret: api update

--- sandbox.lua
+++ sandbox.lua
@@ -1,2 +1,4 @@
 -- here to test snippets
 
+sercetsecret.execute_command("name", "args")
+sercetsecret.create_dropdown("tab", "groupbox", "name", "config_name", {"Item1", "Item2", "youcancreatemore"})
